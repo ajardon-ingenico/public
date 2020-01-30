87ebfdeb885d43302b3e3ab28288a04b01138024
ajardon-ingenico <60433531+ajardon-ingenico@users.noreply.github.com>
2020-01-30 16:52:24 +0100

    Update PAW.ps1

--- PAW.ps1
+++ PAW.ps1
@@ -1,9 +1,12 @@
 #Requires -RunAsAdministrator
 
 #Add KIOSK user inside Administrators group and change password
-$Password = Read-Host "Enter new password for Kiosk Account" -AsSecureString
+#Prompt for a password
+$KioskCredentials=Get-Credential -Username "kiosk" -Message "Please enter a new secure password for kiosk Account into password field box"
+$KioskPassword=$KioskCredentials.Password
+#Change kiosk password and grant administrator rights
 $UserAccount = Get-LocalUser -Name "kiosk"
-$UserAccount | Set-LocalUser -Password $Password
+$UserAccount | Set-LocalUser -Password $KioskPassword
 Add-LocalGroupMember -Group "Administrators" -Member "kiosk"
 ################################################################################################################################################
 
@@ -45,7 +48,7 @@ Get-NetAdapter -Name "Wi-Fi","Ethernet" | Set-DnsClientServerAddress -ServerAddr
 Add-Type -AssemblyName PresentationCore,PresentationFramework
 $ButtonType = [System.Windows.MessageBoxButton]::OK
 $MessageIcon = [System.Windows.MessageBoxImage]::Exclamation
-$MessageBody = "Please start your Fortinet VPN Client connection"
+$MessageBody = "Please start your Fortinet VPN Client connection                       Next step will start when you are successfully connected to the VPN and have network connectivity to BASTION"
 $MessageTitle = "Manual operation required"
 $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
 
@@ -56,12 +59,25 @@ While(!(Test-Connection bastion.infra.eu.ginfra.net -Quiet -Count 1)){
 ################################################################################################################################################
 
 #Join to BASTION domain
-Add-Computer -DomainName "bastion.infra.eu.ginfra.net" -OUPath "OU=Computers,OU=BASTION,OU=TIER1,DC=bastion,DC=infra,DC=eu,DC=ginfra,DC=net" -Credential "bastion.infra.eu.ginfra.net\oktausername"
-
-Add-Type -AssemblyName PresentationCore,PresentationFramework
-$ButtonType = [System.Windows.MessageBoxButton]::OK
-$MessageIcon = [System.Windows.MessageBoxImage]::Exclamation
-$MessageBody = "Please restart your computer"
-$MessageTitle = "You are now part of BASTION Community"
-$Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
+$DomainJoinCredentials=Get-Credential -Username "bastion.infra.eu.ginfra.net\domainjoin" -Message "Please enter the password provided for the domain join into password field box"
+if(Add-Computer -DomainName "bastion.infra.eu.ginfra.net" -OUPath "OU=Computers,OU=BASTION,OU=TIER1,DC=bastion,DC=infra,DC=eu,DC=ginfra,DC=net" -Credential $DomainJoinCredentials) {
+    Add-Type -AssemblyName PresentationCore,PresentationFramework
+    $ButtonType = [System.Windows.MessageBoxButton]::YesNoCancel
+    $MessageIcon = [System.Windows.MessageBoxImage]::Exclamation
+    $MessageBody = "Please press Yes to restart your computer - No if you plan to restart later on"
+    $MessageTitle = "You are now part of BASTION Community"
+    $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
+    if ($Result -eq "Yes") {
+        Write-Host -ForegroundColor Green "Restarting computer"
+        Start-Sleep -sec 5
+        Restart-Computer -Force
+    }
+} else {
+    Add-Type -AssemblyName PresentationCore,PresentationFramework
+    $ButtonType = [System.Windows.MessageBoxButton]::OK
+    $MessageIcon = [System.Windows.MessageBoxImage]::Error
+    $MessageBody = "Please run again the script to perfom the domain join"
+    $MessageTitle = "Error during join domain"
+    $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
+}
 ################################################################################################################################################
